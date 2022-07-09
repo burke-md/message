d271292a23d1cff4583bb9f1a6134ae945b311ba
MB <michael.dburke56@gmail.com>
2022-07-08 20:53:18 -0500

    Implement contract.

--- contracts/Message.sol
+++ contracts/Message.sol
@@ -2,4 +2,13 @@ pragma solidity 0.8.14;
 
 contract Message {
 
+    string message = "";
+
+    function setMessage(string calldata _message) public {
+        message = _message;
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
 }
